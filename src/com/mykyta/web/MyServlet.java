9564d72885cb9d855e1ddf302300651aab7266bf
dmytro <dmitry.b@scopicsoftware.com>
2023-07-10 18:19:34 +0000

    departments servlets

--- src/com/mykyta/web/MyServlet.java
+++ src/com/mykyta/web/MyServlet.java
@@ -13,6 +13,7 @@ public class MyServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         PrintWriter out = resp.getWriter();
         Date date = new Date();
-        out.println(HTML_START + "<h2>Hi There!</h2><br/><h3>Date=" + date + "</h3>" + HTML_END);
+//        throw new RuntimeException();
+        out.println("<html><body><h2>Hi There!</h2><br/><h1>Date=" + date + "</h1></body></html>");
     }
 }
